c22c53a640163a4008ea0db9d020e4917558b0cc
75Tejas <tejm913@gmail.com>
2026-07-19 21:18:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Tejas",
+    author_email="mtejas992@gmail.com",
+    packages=find_packages()
+)
